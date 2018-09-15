bfff264b0b4c50b2a0065f8ef4cb698adb6aebed
Ben Poelstra <bjamin@gmail.com>
2018-09-15 14:55:14 -0600

    Adding a retry if the authorization expires. Cleanup.

--- decora-wifi/session.js
+++ decora-wifi/session.js
@@ -15,19 +15,32 @@ function Session() {
             registeredVia: 'myLeviton'
         };
 
+        this.email = email;
+        this.password = password;
+
         let userData = await this.callApi('/Person/Login', payload, 'POST');
         this.user = new User(this, userData.userId);
         this.authToken = userData.id;
-        console.log(userData);
     }
 
     Session.prototype.callApi = async function (api, payload, method) {
         console.log('calling api', api, method);
+
+        let apiError = async (error) => {
+            console.log(error);
+            if (error.statusCode === 401) {
+                await this.login(this.email, this.password);
+                return await this.callApi(api, payload, method);
+            }
+            throw error;
+        }
+
         if (method === 'GET') {
-            return await request({ url: LEVITON_ROOT + api, method: 'GET', json: true, headers: { authorization: this.authToken } });
+            return await request({ url: LEVITON_ROOT + api, method: 'GET', json: true, headers: { authorization: this.authToken } }).catch(apiError);
         } else {
-            return await request({ url: LEVITON_ROOT + api, method: method, body: payload, json: true, headers: { authorization: this.authToken } });
+            return await request({ url: LEVITON_ROOT + api, method: method, body: payload, json: true, headers: { authorization: this.authToken } }).catch(apiError);
         }
     }
 }
+
 module.exports = Session;
